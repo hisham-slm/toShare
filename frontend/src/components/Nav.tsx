0eae78d5ac2e6a4b10076ff282c6d0b21b12ec84
hisham-slm <hishamsalam350@gmail.com>
2024-10-16 15:10:37 +0400

    feat responsive navbar

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,21 +1,44 @@
 import '../styles/Nav.css'
+import { FaBars } from 'react-icons/fa'
+import { IoClose } from 'react-icons/io5'
 
 import mainLogo from '../assets/logo.png'
+import { useState } from 'react'
 
 function NavBarComponent() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = ()=>{
+        setIsOpen(!isOpen)
+    }
+
+    function renderIcon(isOpen: boolean){
+        if(isOpen){
+            return <IoClose />
+        } else {
+            return <FaBars></FaBars>
+        }
+    }
+
     return (
-        <div className="nav-bar">
-            <div className="left-side">
-                <img src={mainLogo} alt="logo.png" />
-                <h3 className='title'>To Share</h3>
-            </div>
-            <div className="right-side">
-                <a style={{ color : "#feb47b"}} href="/">Home</a>
-                <a href="">New Project</a>
-                <a href="/login">Login</a>
-                <a href="/signup">Sign Up</a>
-            </div>
-        </div >
+        <>
+            <div className="nav-bar">
+                <div className="left-side">
+                    <img src={mainLogo} alt="logo.png" />
+                    <h3 className='title'>To Share</h3>
+                </div>
+                <div className={isOpen ? 'right-side active' : 'right-side'}>
+                    <a style={{ color: "#feb47b" }} href="/">Home</a>
+                    <a href="">New Project</a>
+                    <a href="/login">Login</a>
+                    <a href="/signup">Sign Up</a>
+                </div>
+                <div className="icon" onClick={toggleMenu}>
+                    {renderIcon(isOpen)}
+                </div>
+            </div >
+            
+        </>
     )
 }
 
